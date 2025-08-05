2d050e432ef5ee2425e900517e0f9e2901f1da6a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-08-04 23:08:10 -0300

    Add files via upload

--- reclassificacao_mascaraPRODES_conjunto_B_grupo4.R
+++ reclassificacao_mascaraPRODES_conjunto_B_grupo4.R
@@ -54,6 +54,16 @@ getwd()
 
 ### OBS.: Necessário renomear o nome da imagem .tif da pasta
 
+### Redefinir valor de NA por 3
+
+r <- rast("SENTINEL-2_MSI_034018_2020-01-01_2020-12-18_class_v2.tif")
+unique(values(r))
+unique(values(r)) # Vrificar pixls e máscara
+
+r_mask <- subst(r, NA, 3)
+r_mask[is.na(r_mask[])] <- 3
+unique(values(r_mask)) # Verificar pixels após redefinir NA da máscara por 3
+
 prodes_2020 <- sits_cube(
     source = "BDC",
     collection = "SENTINEL-2-16D",
@@ -64,33 +74,9 @@ prodes_2020 <- sits_cube(
                     "band", "version"),
     bands = "class",
     version = "v2", # Versão do mapa PRODES
-    labels = c("1" = "supressao", "2" = "veg_natural"))
+    labels = c("1" = "supressao", "2" = "veg_natural", "3" = "mascara"))
 
 plot(prodes_2020) # A supressão está dentro da máscara
-
-sits_view(prodes_2020)
-
-# Verificar pixels e valores do mapa PRODES -------------------------------
-
-library(terra)
-
-## Necessário definir o diretório onde está a imagem:
-
-r <- rast("SENTINEL-2_MSI_034018_2020-01-01_2020-12-18_class_v2.tif")
-unique(values(r))
-
-# Resultados dos valores 
-
-# [1,]  NA   # áreas sem dados (geralmente fora do recorte ou mascaradas). 
-## Máscara PRODES, área excluída da análise/classificação.
-# [2,]   2   # provavelmente "Vegetacao"/ Vegetação remanescente ou secundária
-# [3,]   1   # provavelmente "Supressao"
-
-# Mapear a máscara do PRODES
-
-plot(is.na(r), main = "Áreas mascaradas (NA)")
-plot(r) # Aqui a supressao não aparece devido ela estar dentro da máscara
-plot(!is.na(r))
 
 # Reclassificação --------------------------------------------------------------------------------------------------------------------------
 
@@ -101,15 +87,20 @@ getwd()
 caatinga_rec_2020 <- sits_reclassify(
     cube = caatinga_class,
     mask = prodes_2020,
-    rules = list("Mascara" = mask %in% c("NA"),
-    memsize = 10,
+    rules = list("Máscara" = mask %in% c("3"),
+                 "Vegetação" = mask %in% c("2"),
+                 "Supressão" = mask %in% c("1")),
     multicores = 3,
     output_dir = tempdir_r,
-    version = "reclass")
+    version = "reclass3")
 
 ## Todos os dados de desmatamento até 2019 estão sob a máscara do PRODES (em branco).
 ## Fora da máscara aparecem os dados de vegetação e os novos dados de supressão de 2020.
 ## Após inserir a máscara, toda a classificação é feita apenas fora dela.
+
+sits_colors_set(tibble(
+  name = c("supressao", "veg_natural", "mascara"),
+  color = c("brown", "#01665e", "lightblue")))
 
 plot(caatinga_rec_2020,
      legend_text_size = 0.7)
